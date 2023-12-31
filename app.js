01331708f127e46e339baf02e60044c0ab844ad2
DemcharlesB <144748892+DemcharlesB@users.noreply.github.com>
2023-12-31 19:10:53 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+body {
+    font-family: "Times New Roman", sans-serif;
+    background-color: #fff; /* Change to white */
+    color: #000; /* Change text color to black */
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    font-size: 24px;
+  }
+  
+  .container {
+    max-width: 1000px;
+    text-align: center;
+    background-color: #0dbbc4;
+    padding: 80px;
+    box-shadow: 0 0 20px rgba(239, 236, 236, 0.949);
+    transition: box-shadow 0.3s ease;
+  }
+  
+  .container:hover {
+    box-shadow: 0 0 30px rgba(242, 4, 4, 0.968);
+  }
+  
+  h1 {
+    color: #000; /* Change text color to black */
+    margin-bottom: 30px;
+  }
